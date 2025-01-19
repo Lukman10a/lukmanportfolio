acd4346fa31a92acd70e243042ad9ca2d7c22dde
Lukman10a <abdulrauflukman9@gmail.com>
2025-01-19 20:52:36 +0100

    ...

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,16 +4,15 @@ import ContactForm from "./form";
 
 export default function Footer() {
   return (
-    <div className="bg-[#181414] p-20 sm:p-8 text-white grid grid-cols-2 gap-6 sm:grid-cols-1">
+    <div className="bg-black p-20 sm:p-8 text-white grid grid-cols-2 gap-6 sm:grid-cols-1">
       <div className="space-y-7">
-        <p className="text-xl">Lukman</p>
         <h3 className="text-[80px] sm:text-7xl">
           Get In Touch <br /> With Us
         </h3>
         <p className="text-4xl hover:text-[#3939cf] cursor-pointer sm:text-xl">
           abdulrauflukman9@gmail.com
         </p>
-        <div className="flex justify-between">
+        <div className="flex gap-10">
           <Link href={"https://github.com/Lukman10a/"}>
             <p>Github</p>
           </Link>
